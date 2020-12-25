32d5d5af9d83ff18fb31fb39b6bb09764e03561f
Briard <briard1996@gmail.com>
2020-12-25 13:16:36 -0500

    Se agregan los metodos de get, put, post

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const bodyParser = require('body-parser');
+
 
 
 // parse application/x-www-form-urlencoded
@@ -10,32 +11,17 @@ app.use(express.urlencoded({ extended: false }));
 // parse application/json
 app.use(express.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
 
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({ persona: body });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
+    console.log("Base de datos Online!");
 });
 
 app.listen(process.env.PORT, () => {
