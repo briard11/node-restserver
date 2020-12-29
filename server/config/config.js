37339e465db9b2f896d1073facfe259f9ef072ac
briard <briard1996@gmail.com>
2020-12-28 22:57:58 -0500

    Variable entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,8 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://briard:wQAsr8Sbcya4inL2@cluster0.imqdp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
-//process.env.URLDB = 'mongodb+srv://briard:wQAsr8Sbcya4inL2@cluster0.imqdp.mongodb.net/cafe';
